115ee7a6a662c47b79fbba5b6d3dccad26f0742d
Devashish2910 <dgpatel2910@gmail.com>
2018-03-19 00:34:27 -0500

    modified: motion graph

--- plotting.py
+++ plotting.py
@@ -1,13 +1,20 @@
 from motion_detector import df
 from bokeh.plotting import figure, output_file, show
+from bokeh.models import HoverTool, ColumnDataSource
 
+df["Start_String"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_String"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
 
+cds = ColumnDataSource(df)
 # Create a figure
 p = figure(x_axis_type='datetime', title="Motion graph", background_fill_color="#EFE8E2",sizing_mode='scale_width', height=100, width=470)
 p.yaxis.minor_tick_line_color = None
 p.yaxis[0].ticker.desired_num_ticks=1
 
-p.quad(left=df['Start'], right=df['End'], bottom=0, top=1, fill_color="#3B8686")
+hover = HoverTool(tooltips=[("Start",'@Start_String'), ("End",'@End_String')])
+p.add_tools(hover)
+
+p.quad(left='Start', right='End', bottom=0, top=1, fill_color="#3B8686", source=cds)
 
 # Save it to file
 output_file("Chart.html")
